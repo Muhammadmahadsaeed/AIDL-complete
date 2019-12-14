4698da8e2793a50af2294ef2ab1ac77bcb5e7bf3
mahadkhan809@gmail.com <mahadkhan809@gmail.com>
2019-12-14 13:28:49 +0500

    blog

--- Dashboard/Dashboard/pages/examples/viewBlog.php
+++ Dashboard/Dashboard/pages/examples/viewBlog.php
@@ -1,11 +1,31 @@
 <?php require_once('../../config/db.php');
-$display = "select * from blog where blog_id = 1";
+$display = "select * from blog where blog_id = 6";
 $res = mysqli_query($link,$display);
 
-while ($result = mysqli_fetch_array($res)) {
-    @header('Content-type: text/html; charset=utf-8'); 
-    echo $result['blog'];
+
+    // @header('Content-type: text/html; charset=utf-8'); 
    
-   }
+    
+   
+   
 
 ?>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+</head>
+
+<body>
+    <div class="container">
+       <?php while ($result = mysqli_fetch_array($res)) {
+        echo $result['blog'];
+        }?>
+    </div>
+</body>
+
+</html>
